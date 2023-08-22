5be8d180a8e9abc8e4fa7a197c5b6edf0bae1b9c
WindaLobo <lobo.winsus@gmail.com>
2023-08-22 11:23:14 +0200

    Curso C#

---
+++ Programacion/VisualEstudio/Ejemplos/Variables/Variables/CursoCSharp_9_for/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CursoCSharp_9_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numero = 10;
+            //for (int i = 0; i < numero; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            int numero2 = 0;
+            for (int i = 10; i > numero2; i--)
+            {
+                Console.WriteLine(i);
+            }    
+        }
+    }
+}
---
+++ Programacion/VisualEstudio/Ejemplos/Variables/Variables/CursoCsharp_10_foreach/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CursoCsharp_10_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var listaDeNumero = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //foreach (int numero in listaDeNumero)
+            //{
+            //    Console.WriteLine(numero);
+            //}
+
+            var listaDePersonas = new List<Persona>();
+            var persona1 = new Persona()
+            {
+                name = "Windar Sujey",
+                lastname = "Lobo Garces",
+                edad = 30,
+            };
+            var persona2 = new Persona()
+            {
+                name = "Adrian",
+                lastname = "Estevez Gayoso",
+                edad = 32,
+
+            };
+            listaDePersonas.Add(persona1);
+            listaDePersonas.Add (persona2);
+
+
+            foreach (var persona in listaDePersonas)
+            {
+                Console.WriteLine(persona.name+ " "+ persona.lastname + " tiene " + persona.edad + " años");
+
+            }
+
+        }
+        public class Persona
+        {
+
+            public string name { get; set; }
+            public string lastname { get; set; }
+            public int edad { get; set; }
+        }
+
+    }
+
+
+}
