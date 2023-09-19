02d8718b7a797007ff9ad1475f7b4e72f210eebd
WindarLoboGarces <windar.lobo.garces@ciclosmontecastelo.com>
2023-09-19 12:27:28 +0200

    Terminado boletín 4

---
+++ Programacion/VisualEstudio/Ejercicios/Boletin_4/Ejercicios/Ejercicio_Uno_/Ejercicio_Cinco_/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+namespace Ejercicio_Cinco_;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*Crear un programa que muestre las letras de la Z (mayúscula) a la A (mayúscula, descendiendo)*/
+
+        char letra = 'Z'; // Comienza con la letra 'z'
+
+        while (letra >= 'A')
+        {
+            Console.WriteLine(letra);
+            letra--;
+        }
+    }
+}
