14a4158a767eee9a10956273684ba985ca5fa8bb
WindarLoboGarces <windar.lobo.garces@ciclosmontecastelo.com>
2023-09-21 13:56:27 +0200

    Terminado el boletín 5

---
+++ Programacion/VisualEstudio/Ejercicios/Boletin_5/Ejercicios/Ejercicio_Uno/Ejercicio_Cinco/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Drawing;
+using static System.Console;
+namespace Ejercicio_Cinco;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*Usando Arrays, crea un programa que nos pida cuantos elementos tendrá el array, 
+          dependiendo de ese numero de elementos, nos ira pidiendo valores, que introduciremos en el array. 
+          una vez introducidos,
+         nos pedirá un nuevo valor a insertar y la posición donde queremos insertarla en el array.*/
+        do
+        {
+
+            WriteLine("Cuanto elemento quieres tener tu array");
+            string lis = ReadLine();
+            if (int.TryParse(lis, out int tamañoArrays) && tamañoArrays > 0)
+            {
+
+                int[] miArreglo = new int[tamañoArrays];
+
+                for (int i = 0; i < tamañoArrays; i++)
+                {
+                    Write($"Ingrese el elemento {i + 1}: ");
+                    string arreglo = ReadLine();
+
+                    if (int.TryParse(arreglo, out int elemento))
+                    {
+                        miArreglo[i] = elemento;
+
+                    }
+                    else
+                    {
+                        WriteLine("Invalido, debes de ingresar un numero entero");
+                        i--;
+                    }
+                }
+                WriteLine("Elementos en el arreglo:");
+                foreach (int elemento in miArreglo)
+                {
+                    WriteLine(elemento);
+                   
+                }
+                break;
+
+            }
+            else
+            {
+                WriteLine("Error al introducir el elemento,debes de ingresar un numero entero");
+            }
+          
+        } while (true);
+    }
+}
---
+++ Programacion/VisualEstudio/Ejercicios/Boletin_5/Ejercicios/Ejercicio_Uno/Ejercicio_Cuatro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using static System.Console;
+namespace Ejercicio_Cuatro;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*Crear un programa nos pida introducir 10 números enteros. 
+         esos valores los introduciremos en un Listado y  mostraremos el numero mayor y menor, 
+          consultando los valores en el Listado*/
+
+        ArrayList listaDeNumeros = new ArrayList();
+
+        int numero = 10;
+
+        for (int i = 1; i <= numero; i++)
+        {
+            WriteLine($"Introduce el {i} numero : ");
+            string usuario = ReadLine();
+
+            if (int.TryParse(usuario, out int numeroUsuario))
+            {
+                listaDeNumeros.Add(numeroUsuario);
+                listaDeNumeros.Sort();
+                listaDeNumeros.Reverse();
+            }
+
+        }
+        WriteLine($"\nMostraremos los  numeros de  mayor y menor :");
+        ReadLine();
+        foreach (int i in listaDeNumeros)
+        {
+
+            WriteLine(i);
+
+
+        }
+        ReadLine();
+    }
+}
